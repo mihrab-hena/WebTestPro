eaecc58cc8351c11c833854d6a3809ed91a306da
mihrab-hena <mihrabhena@gmail.com>
2018-02-25 15:05:48 +0100

    validation

--- WebTestPro/App_Code/DAL/UserDataBaseHandler.cs
+++ WebTestPro/App_Code/DAL/UserDataBaseHandler.cs
@@ -11,11 +11,13 @@ using System.Web;
 /// </summary>
 public class UserDataBaseHandler
 {
+    private bool successStatus;
     public UserDataBaseHandler()
     {
         //
         // TODO: Add constructor logic here
         //
+        successStatus = false;
     }
 
     public bool InsertUserDataToDB(User anUser)
@@ -29,7 +31,7 @@ public class UserDataBaseHandler
                                " VALUES(@FirstName,@LastName,@Gender,@Phone,@Email,@StreetAddress,@PostCode,@District,@Country);";
         sqlComnd.Parameters.AddWithValue("@FirstName",anUser.FirstName);
         sqlComnd.Parameters.AddWithValue("@LastName",anUser.LastName);
-        sqlComnd.Parameters.AddWithValue("@Gender","Male");
+        sqlComnd.Parameters.AddWithValue("@Gender",anUser.Gender);
         sqlComnd.Parameters.AddWithValue("@Phone",anUser.PhoneNumber);
         sqlComnd.Parameters.AddWithValue("@Email",anUser.Email);
         sqlComnd.Parameters.AddWithValue("@StreetAddress", anUser.StreetNumber + " "+ anUser.StreetAddress);
@@ -41,6 +43,7 @@ public class UserDataBaseHandler
         {
             sqlConnection.Open();
             sqlComnd.ExecuteNonQuery();
+            successStatus = true;
         }
         finally
         {
@@ -52,6 +55,26 @@ public class UserDataBaseHandler
                 }
             }
         }
+        return successStatus;
+    }
+
+    public bool UpdateUserDataToDB(User anUser)
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["DBHotel"].ConnectionString;
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+        SqlCommand sqlComnd = new SqlCommand();
+        sqlComnd.Connection = sqlConnection;
+
+        sqlComnd.Parameters.AddWithValue("@FirstName", anUser.FirstName);
+        sqlComnd.Parameters.AddWithValue("@LastName", anUser.LastName);
+        sqlComnd.Parameters.AddWithValue("@Gender", anUser.Gender);
+        sqlComnd.Parameters.AddWithValue("@Phone", anUser.PhoneNumber);
+        sqlComnd.Parameters.AddWithValue("@Email", anUser.Email);
+        sqlComnd.Parameters.AddWithValue("@StreetAddress", anUser.StreetNumber + " " + anUser.StreetAddress);
+        sqlComnd.Parameters.AddWithValue("@PostCode", anUser.PostCode);
+        sqlComnd.Parameters.AddWithValue("@District", anUser.District);
+        sqlComnd.Parameters.AddWithValue("@Country", anUser.Country);
+
         return true;
     }
 }
