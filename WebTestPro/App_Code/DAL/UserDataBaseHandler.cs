caf1993fdd37a02eab91dfb7a89f409b4c9a51d5
mihrab-hena <mihrabhena@gmail.com>
2018-02-28 19:39:27 +0100

    few changes in DAL

--- WebTestPro/App_Code/DAL/UserDataBaseHandler.cs
+++ WebTestPro/App_Code/DAL/UserDataBaseHandler.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Web;
 
 /// <summary>
-/// Summary description for UserDataBaseHandler
+/// UserDataBaseHandler class provides support for hendling data of users from Database
 /// </summary>
 public class UserDataBaseHandler
 {
@@ -76,5 +76,55 @@ public class UserDataBaseHandler
         sqlComnd.Parameters.AddWithValue("@Country", anUser.Country);
 
         return true;
+    }
+
+    public List<User> GetAllUsers()
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["DBHotel"].ConnectionString;
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+        SqlCommand sqlComnd = new SqlCommand();
+        sqlComnd.Connection = sqlConnection;
+        SqlDataReader sqlDataReader = null;
+        List<User> userList = new List<User>();
+        sqlComnd.CommandText = "SELECT TOP (20) [UserID], [FirstName] ,[LastName]" +
+                                ",[Gender] ,[Phone] ,[Email] ,[StreetAddress] ,[PostCode]" +
+                                ",[District] ,[Country] FROM[htlMng].[dbo].[UserInfo]";
+        try
+        {
+            sqlConnection.Open();
+            sqlDataReader = sqlComnd.ExecuteReader();
+            
+            while (sqlDataReader.Read())
+            {
+                User aUser = new User();
+                
+                aUser.FirstName = (sqlDataReader["FirstName"] ?? "" ).ToString();
+                aUser.LastName = (sqlDataReader["LastName"] ?? "" ).ToString();
+                aUser.Gender = (sqlDataReader["Gender"] ?? "" ).ToString();
+                aUser.PhoneNumber = (sqlDataReader["Phone"] ?? "" ).ToString();
+                aUser.Email = (sqlDataReader["Email"] ?? "" ).ToString();
+                aUser.StreetNumber = "";
+                aUser.StreetAddress = (sqlDataReader["StreetAddress"] ?? "" ).ToString();
+                aUser.PostCode = (sqlDataReader["PostCode"] ?? "" ).ToString();
+                aUser.District = (sqlDataReader["District"] ?? "" ).ToString();
+                aUser.Country = (sqlDataReader["Country"] ?? "" ).ToString();
+                userList.Add(aUser);
+            }
+        }
+        finally
+        {
+            if (sqlConnection != null)
+            {
+                if (sqlConnection.State == ConnectionState.Open)
+                {
+                    sqlConnection.Close();
+                }
+            }
+            if (sqlDataReader != null)
+            {
+                sqlDataReader.Close();
+            }
+        }
+        return userList;
     }
 }
